8fad4cc4f18492df1bf528f97021f970c3baeb10
OksanaTishchenko <oksana.tishchenko@ukr.net>
2021-02-24 12:37:38 +0200

    add requests and output rockets

--- src/App.js
+++ src/App.js
@@ -7,17 +7,56 @@ import Footer from './components/Footer/Footer';
 import Calendar from './components/Calendar/Calendar';
 import Details from './components/Details/Details';
 
+import FetchData from './service/FetchData';
+
 import './style.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Main />
-      <Features />
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+  };
+
+  componentDidMount() {
+    // this.fetchData.getRocket().then(data => console.log(data))
+    this.updateRocket();
+    console.log(this.state);
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) })
+        return data;
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(features => {
+        this.setState({ rocketFeatures: features });
+      });
+  }
+
+  changeRocket = (nameRocket) => {
+    this.setState({ rocket: nameRocket }, this.updateRocket); // вызываем updateRocket потому что поменялись свойства, и нужно обновить данные т.к. в стейте рокет поменялся 
+
+  }
+
+  render() {
+
+    //console.log(this.state);
+
+    return (
+      <React.Fragment>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+        <Main rocket={this.state.rocket} />
+        <Features />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './Header.css';
 
-const Header = () => (
+const Header = (props) => (
   <header className="header">
     <img
       src={logo}
@@ -11,24 +11,36 @@ const Header = () => (
     />
     <nav className="main-nav nav">
       <ul className="list">
-        <li className="item">
-          <a href="#" className="item-link">Falcon 1</a>
+        {props.rockets.map((item, index) => {
+          return (
+            <li key={index} className="item">
+              <a href="/"
+                onClick={e => {
+                  e.preventDefault();
+                  props.changeRocket(item);
+                }}
+                className="item-link">{item}</a>
+            </li>
+          )
+        })}
+        {/* <li className="item">
+          <a href="/" className="item-link">Falcon 1</a>
         </li>
         <li className="item">
-          <a href="#" className="item-link">Falcon 9</a>
+          <a href="/" className="item-link">Falcon 9</a>
         </li>
         <li className="item">
-          <a href="#" className="item-link">Falcon Heavy</a>
+          <a href="/" className="item-link">Falcon Heavy</a>
         </li>
         <li className="item">
-          <a href="#" className="item-link">Updates</a>
-        </li>
+          <a href="/" className="item-link">Updates</a>
+        </li> */}
       </ul>
     </nav>
     <nav className="secondary-nav">
       <ul className="list">
         <li className="item">
-          <a href="#" className="item-link">Home</a>
+          <a href="/" className="item-link">Home</a>
         </li>
         <li className="item">
           <a href="calendar.html" className="item-link">Calendar</a>
